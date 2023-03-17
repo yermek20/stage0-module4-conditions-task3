fd8124b48aa94fd22425dc284a04f471b05b016b
Yermek <yermek20@gmail.com>
2023-03-18 01:01:11 +0600

    Module4 Task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char c = character;
+        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+            System.out.println("Vowel");
+        } else {
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                System.out.println("Consonant");
+            } else {
+                System.out.println("wrong alphabet!");
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary<=10000 && salary>0) {
+            System.out.println((double) (salary*85/100));
+        } else if (salary<=20000 && salary>0) {
+            System.out.println((double) (salary*82/100));
+        } else if (salary>20000 && salary>0) {
+            System.out.println((double) (salary*80/100));
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
